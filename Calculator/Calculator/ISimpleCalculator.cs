c04f7a703148b8ec56663066dc42a288ffa760f4
qwertyhost <Pavitar Singh>
2021-10-14 10:22:45 +0100

    f

---
+++ Calculator/Calculator/ISimpleCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public interface ISimpleCalculator
+    {
+        int Add(int start,int amount);
+        int Subtract(int start, int amount);
+        int Multiply(int start, int amount);
+        int Divide(int start, int amount);
+        
+    }
+}
